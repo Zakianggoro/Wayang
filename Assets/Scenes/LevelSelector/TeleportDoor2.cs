9ede8c433630dde0c7526047fdc3649658a307a8
Phoenixops <mzakianggoro324@gmail.com>
2024-05-24 09:23:53 +0700

    Level Selector Update + Door

---
+++ Assets/Scenes/LevelSelector/TeleportDoor2.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TeleportDoor2 : MonoBehaviour
+{
+    private bool playerInRange;
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(4); // Change 3 to the appropriate scene index for Building Index 3
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
