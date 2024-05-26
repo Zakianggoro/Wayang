26c88ca405cbe0314de7c37483d6644ae32a7d7d
HzardGenmu <161951115+HzardGenmu@users.noreply.github.com>
2024-05-26 19:53:30 +0700

    Last Prototype Update

---
+++ Assets/Scenes/LevelSelector/Theater 1/Story 1/ToKarna.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToKarna : MonoBehaviour
+{
+    private bool playerInRange;
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            SceneManager.LoadScene(9);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
